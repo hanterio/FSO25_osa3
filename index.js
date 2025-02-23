91974cc6e6f3834387a740bacc14272ba7f05dda
Hannes Vieth <hannesvieth@gmail.com>
2025-02-23 14:52:52 +0200

    Tehtävät 3.4-3.6 tehty

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     {
         "name": "Arto Hellas",
@@ -45,6 +47,46 @@ app.get('/api/persons/:id', (request, response) => {
         response.status(404).end()
       }
 })
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    persons = persons.filter(person => person.id !== id)
+  
+    response.status(204).end()
+  })
+
+app.post('/api/persons', (request, response) => {
+    const uusiId = persons.length > 0
+        ? Math.floor(Math.random() * 1000000000) 
+        : 1
+
+    const body = request.body
+
+    if (!body.name) {
+        return response.status(400).json({
+            error: 'nimi puuttuu'
+        })
+    } else if (!body.number) {
+        return response.status(400).json({
+            error: 'numero puuttuu'
+        })
+    } else if (persons.some(person => person.name === body.name)) {
+        return response.status(400).json({
+            error: 'nimi on jo luettelossa'
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: String(uusiId)
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
+})
+
 
 const PORT = 3001
 app.listen(PORT, () => {
