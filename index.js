6b730119793576c9d51f3290553512930f9fa848
Hannes Vieth <hannesvieth@gmail.com>
2025-03-09 22:08:31 +0200

    calidoinnit hoidettu eli tehtävät 3.19-3.21 tehty. Siirretään tuotantoon.

--- index.js
+++ index.js
@@ -70,9 +70,11 @@ app.post('/api/persons', (request, response, next) => {
       number: body.number || false,
     })
   
-    person.save().then(savedPerson => {
-      response.json(savedPerson)
+    person.save()
+        .then(savedPerson => {
+            response.json(savedPerson)
     })
+        .catch(error => next(error))
   })
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -105,8 +107,10 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json(( error.message ))
     }
-
+    
     next(error)
 }
 
--- models/person.js
+++ models/person.js
@@ -15,8 +15,22 @@ mongoose.connect(url)
 
 
 const puhelinSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minlength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minlength: 8,
+    validate: {
+        validator: function(v) {
+            return /^\d{2}-\d{6,}$/.test(v) || /^\d{3}-\d{5,}$/.test(v)
+        },
+        message: props => "numeron pitäisi olla muotoa XX-XXXXXX tai XXX-XXXXXX"
+    },
+    required: true
+  },
 })
 
 puhelinSchema.set('toJSON', {
